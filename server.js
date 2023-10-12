1ca2bfc5c5a54a684af9407f35717d82f3dcbedf
jorgegoco <jorgegoco70@gmail.com>
2023-10-12 13:55:25 +0200

    Add static views

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const app = express()
+const path = require("path")
 const cors = require("cors")
 const { logger } = require("./middleware/logEvents")
 const errorHandler = require("./middleware/errorHandler")
@@ -10,6 +11,27 @@ app.use(logger)
 
 // Cross Origin Resource Sharing
 app.use(cors())
+
+// built-in middleware for json
+app.use(express.json())
+
+//serve static files
+app.use("/", express.static(path.join(__dirname, "/public")))
+
+// routes
+app.use("/", require("./routes/root"))
+
+app.all("*", (req, res) => {
+  res.status(404)
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"))
+  } else if (req.accepts("json")) {
+    res.json({ error: "404 Not Found" })
+  } else {
+    res.type("txt").send("404 Not Found")
+  }
+})
+
 app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
