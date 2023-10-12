41d48294ab216b4cfd0e28a14910ba4040637309
jorgegoco <jorgegoco70@gmail.com>
2023-10-12 15:05:04 +0200

    Add user registration in local database

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ app.use("/", express.static(path.join(__dirname, "/public")))
 
 // routes
 app.use("/", require("./routes/root"))
+app.use("/register", require("./routes/register"))
 
 app.all("*", (req, res) => {
   res.status(404)
