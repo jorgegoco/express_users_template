565af446fb17a1bf5a3a8dbf003977c8cae5f213
jorgegoco <jorgegoco70@gmail.com>
2023-10-12 12:58:35 +0200

    Add middleware

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 const express = require("express")
 const app = express()
+const cors = require("cors")
+const { logger } = require("./middleware/logEvents")
+const errorHandler = require("./middleware/errorHandler")
 const PORT = process.env.PORT || 3500
+
+// custom middleware logger
+app.use(logger)
+
+// Cross Origin Resource Sharing
+app.use(cors())
+app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
