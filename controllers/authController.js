442e6ca8b4f6c2dbebf993df7c777bfbb2f88764
jorgegoco <jorgegoco70@gmail.com>
2023-10-19 21:16:44 +0200

    Fix the auth controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,6 +3,7 @@ const bcrypt = require("bcrypt")
 const jwt = require("jsonwebtoken")
 
 const handleLogin = async (req, res) => {
+  const cookies = req.cookies
   const { user, pwd } = req.body
   if (!user || !pwd)
     return res
@@ -20,17 +21,25 @@ const handleLogin = async (req, res) => {
       process.env.ACCESS_TOKEN_SECRET,
       { expiresIn: "10s" }
     )
-    const refreshToken = jwt.sign(
+    const newRefreshToken = jwt.sign(
       { username: foundUser.username },
       process.env.REFRESH_TOKEN_SECRET,
       { expiresIn: "15s" }
     )
+
+    let newRefreshTokenArray = !cookies?.jwt
+      ? foundUser.refreshToken
+      : foundUser.refreshToken.filter((rt) => rt !== cookies.jwt)
+
+    if (cookies?.jwt)
+      res.clearCookie("jwt", { httpOnly: true, sameSite: "None", secure: true })
+
     // Saving refreshToken with current user
-    foundUser.refreshToken = refreshToken
+    foundUser.refreshToken = [...newRefreshTokenArray, newRefreshToken]
     const result = await foundUser.save()
     console.log(result)
 
-    res.cookie("jwt", refreshToken, {
+    res.cookie("jwt", newRefreshToken, {
       httpOnly: true, // This avoids any foreign javascript code to have access to that cookie
       sameSite: "Lax",
       // secure: true, // when working in dev mode, like thunderclient, this must be deleted to accept cookies with http
